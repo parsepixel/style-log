3238688ba34ebf330478b5f0d37f575bdb7b23f4
Kevin Gecsi <kevin.gecsi@gmail.com>
2021-08-31 05:12:41 -0400

    Recompile dist locally

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require('laravel-mix');
 
 mix.js('src/style-log.js', 'dist/style-log.js')
     .setPublicPath('dist')
+    // .sourceMaps()
     // .version()
     // .options({
     //     processCssUrls: false
